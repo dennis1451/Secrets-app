ef59d13e21448bbcf02e45e601a0d551767b02e8
dennis1451 <dennis1451@gmail.com>
2020-08-29 09:22:37 +0300

    Adding level 3 Security, MD5

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser")
 const ejs = require ("ejs")
 const mongoose = require("mongoose")
 const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
 
 const app = express()
 
@@ -23,8 +24,6 @@ const userSchema = new mongoose.Schema({
   password: String
 })
 
-const secret = "Thisisourlittlesecret."
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]})
 
 const User = new mongoose.model("User", userSchema)
 
@@ -44,7 +43,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   })
   newUser.save(function(err){
     if(err){
